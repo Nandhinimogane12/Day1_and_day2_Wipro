7bc7b58fd13c0466b1dd1132235fe38e29c51a66
Nandhinimogane12 <147993979+Nandhinimogane12@users.noreply.github.com>
2026-04-22 21:31:00 +0530

    Add files via upload

--- day5/Exceptional_Handling/Program.cs
+++ day5/Exceptional_Handling/Program.cs
@@ -1,1 +1,32 @@
+﻿using System;
+class AgeException : Exception
+{
+    public AgeException(string message) : base(message) { }
+}
 
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        try//exception thrown
+        {
+            if (age < 18)
+            {
+                throw new AgeException("Age must be 18 or above.");
+            }
+
+            Console.WriteLine("Valid age.");
+        }
+        catch (AgeException ae) //catches the exception
+        {
+            Console.WriteLine("Error: " + ae.Message);
+        }
+        finally //will always execute.
+        {
+            Console.WriteLine("Exceuted");
+        }
+    }
+}
