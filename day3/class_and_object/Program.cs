c0520e561f3e1af4e6089d7574fc13daf38b2cd4
Nandhinimogane12 <147993979+Nandhinimogane12@users.noreply.github.com>
2026-04-22 18:10:52 +0530

    Add files via upload

--- day3/class_and_object/Program.cs
+++ day3/class_and_object/Program.cs
@@ -1,1 +1,57 @@
+﻿using System;
+
+class Book
+{
+    public string Title;
+    public string Author;
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Title: " + Title);
+        Console.WriteLine("Author: " + Author);
+    }
+}
+
+class Student
+{
+    public string Name;
+    public int Age;
+
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void DisplayStudent()
+    {
+        Console.WriteLine("Student Name: " + Name);
+        Console.WriteLine("Student Age: " + Age);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Book object
+        Book myBook = new Book("Ponniyin Selvan", "Kalki");
+        myBook.DisplayInfo();
+
+        Console.WriteLine();
+
+        // Student object
+        Student s1 = new Student("Nandhini", 21);
+        s1.DisplayStudent();
+        Console.ReadLine();
+    }
+}
+
+
 
