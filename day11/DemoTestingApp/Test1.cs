290fc48339ebf1ddc80f77197785dca0e8ae2858
Nandhinimogane12 <147993979+Nandhinimogane12@users.noreply.github.com>
2026-04-28 11:24:11 +0530

    Add files via upload

--- day11/DemoTestingApp/Test1.cs
+++ day11/DemoTestingApp/Test1.cs
@@ -1,1 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoTestingApp;
 
+namespace Demo.Tests;
+
+[TestClass]
+public class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var Calc = new Calculator();
+        int result = Calc.Multiply(2, 6);
+        Assert.AreEqual(12, result);
+    }
+}
