d0a81dbb99495eca60b34eed64ec751b1ee9bd1d
Nandhinimogane12 <147993979+Nandhinimogane12@users.noreply.github.com>
2026-04-28 11:03:44 +0530

    Add files via upload

--- day12/DigitalWalletManagementSystemUsingSOLIDPrinciples/Program.cs
+++ day12/DigitalWalletManagementSystemUsingSOLIDPrinciples/Program.cs
@@ -1,1 +1,30 @@
+﻿using System;
 
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LSP Demonstration
+            IPayment payment = new CardPayment();
+            payment.Pay();
+
+            payment = new UPIPayment();
+            payment.Pay();
+
+            payment = new NetBankingPayment();
+            payment.Pay();
+
+            Console.WriteLine("------------------");
+
+            // DIP Demonstration
+            INotification notification = new EmailNotification();
+
+            WalletService wallet = new WalletService(payment, notification);
+            wallet.ProcessPayment();
+
+            Console.ReadLine();
+        }
+    }
+}
