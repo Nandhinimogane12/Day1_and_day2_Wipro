7a3056e37af58be327fac2ad6626b5c0ac10b86e
Nandhinimogane12 <147993979+Nandhinimogane12@users.noreply.github.com>
2026-04-28 11:06:10 +0530

    Add files via upload

--- day12/DigitalWalletManagementSystemUsingSOLIDPrinciples/WalletService.cs
+++ day12/DigitalWalletManagementSystemUsingSOLIDPrinciples/WalletService.cs
@@ -1,1 +1,118 @@
+﻿using System;
 
+namespace ConsoleApp1
+{
+    // Poor Design (Violating SOLID)
+    internal class MyWalletService
+    {
+        public void ProcessPayment1()
+        {
+            Console.WriteLine("Processing Payment");
+
+            // Payment Logic
+            Console.WriteLine("Payment done using Card");
+
+            // Notification Logic
+            Console.WriteLine("Email Notification Sent");
+
+            // Save Transaction
+            Console.WriteLine("Transaction Saved");
+        }
+    }
+
+    // Step 1 : SRP (Single Responsibility Principle)
+
+    public class TransactionService
+    {
+        public void SaveTransaction()
+        {
+            Console.WriteLine("Transaction Saved Successfully");
+        }
+    }
+
+    public class PaymentService
+    {
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment Completed");
+        }
+    }
+
+    public class NotificationService
+    {
+        public void NotifyUser()
+        {
+            Console.WriteLine("Notification Sent");
+        }
+    }
+
+    // Step 2 : OCP + DIP
+    // IMPORTANT: make interface PUBLIC
+
+    public interface IPayment
+    {
+        void Pay();
+    }
+
+    public class CardPayment : IPayment
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Payment done using Card");
+        }
+    }
+
+    public class UPIPayment : IPayment
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Payment done using UPI");
+        }
+    }
+
+    public class NetBankingPayment : IPayment
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Payment done using Net Banking");
+        }
+    }
+
+    // Step 3 : ISP
+    // IMPORTANT: make interface PUBLIC
+
+    public interface INotification
+    {
+        void SendMessage();
+    }
+
+    public class EmailNotification : INotification
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Email Notification Sent");
+        }
+    }
+
+    // Step 4 : DIP
+
+    public class WalletService
+    {
+        private IPayment _payment;
+        private INotification _notification;
+
+        public WalletService(IPayment payment, INotification notification)
+        {
+            _payment = payment;
+            _notification = notification;
+        }
+
+        public void ProcessPayment()
+        {
+            _payment.Pay();
+            _notification.SendMessage();
+
+            Console.WriteLine("Wallet Payment Processed Successfully");
+        }
+    }
+}
