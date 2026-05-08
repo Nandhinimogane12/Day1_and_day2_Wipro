8a8f993e9f6c5153fef8a358edaf3de2d37e152e
Nandhinimogane12 <nandhinimogane74@gmail.com>
2026-05-08 15:54:09 +0530

    Day 19 - Online Exam Portal MVC with Filters

---
+++ day19/OnlineExamPortal/OnlineExamPortal/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineExamPortal.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username)
+        {
+            HttpContext.Session.SetString("User", username);
+
+            return RedirectToAction("Dashboard");
+        }
+
+        public IActionResult Dashboard()
+        {
+            var user = HttpContext.Session.GetString("User");
+
+            return Content("Welcome " + user);
+        }
+
+        public IActionResult SetCookie()
+        {
+            Response.Cookies.Append("Theme", "Dark");
+
+            return Content("Cookie Created");
+        }
+
+        public IActionResult GetCookie()
+        {
+            var theme = Request.Cookies["Theme"];
+
+            return Content("Theme is " + theme);
+        }
+    }
+}
