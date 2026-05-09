9b110be12581a87ec5c483efedeacedc0b4f713c
Nandhinimogane12 <nandhinimogane74@gmail.com>
2026-05-09 17:28:59 +0530

    Day 21: Added CollegeApplication - ADO.NET Student DB

---
+++ day21/CollegeApplication/CollegeApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Connection String
+        string connectionString = "Server=NANDHINI\\SQLEXPRESS;Database=Student;Trusted_Connection=True;TrustServerCertificate=True;";
+        // Create connection
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connected Successfully!");
+                // SQL Query
+                string query = "SELECT * FROM Students";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader["Id"]}, Name: {reader["Name"]}, Email: {reader["Email"]}");
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
