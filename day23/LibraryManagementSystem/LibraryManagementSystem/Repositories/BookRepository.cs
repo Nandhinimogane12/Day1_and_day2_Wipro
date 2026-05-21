e2e778af424f965100283a56a76bbcf3e74bad03
Nandhinimogane12 <nandhinimogane74@gmail.com>
2026-05-21 10:48:00 +0530

    Day 23: Add LibraryManagementSystem project

---
+++ day23/LibraryManagementSystem/LibraryManagementSystem/Repositories/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Models;
+using LibraryManagementSystem.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class BookRepository : GenericRepository<Book>, IBookRepository
+    {
+        private readonly LibraryDbContext _context;
+
+        public BookRepository(LibraryDbContext context)
+            : base(context)
+        {
+            _context = context;
+        }
+
+        // SEARCH BOOKS
+
+        public async Task<IEnumerable<Book>> SearchBooks(string search)
+        {
+            return await _context.Books
+                .Where(b => b.Title.Contains(search))
+                .ToListAsync();
+        }
+
+        // PAGINATION
+
+        public async Task<IEnumerable<Book>> GetPagedBooks(int pageNumber, int pageSize)
+        {
+            return await _context.Books
+                .OrderBy(b => b.Id)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+    }
+}
---
+++ day23/LibraryManagementSystem/LibraryManagementSystem/Repositories/Interfaces/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Repositories.Interfaces
+{
+    public interface IBookRepository : IGenericRepository<Book>
+    {
+        Task<IEnumerable<Book>> SearchBooks(string search);
+
+        Task<IEnumerable<Book>> GetPagedBooks(int pageNumber, int pageSize);
+    }
+}
