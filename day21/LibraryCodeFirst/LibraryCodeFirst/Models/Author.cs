9dd5345d431df22df3878afa9a070f7afa895de0
Nandhinimogane12 <nandhinimogane74@gmail.com>
2026-05-21 11:17:50 +0530

    Day 21: Remove LibraryCodeFirst folder

--- day21/LibraryCodeFirst/LibraryCodeFirst/Models/Author.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LibraryCodeFirst.Models
-{
-    public class Author
-    {
-        public int AuthorID { get; set; } // Capital ID
-        public string Name { get; set; }
-        public ICollection<Book> Books { get; set; } = new List<Book>();
-    }
-}
--- day21/LibraryCodeFirst/LibraryCodeFirst/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LibraryCodeFirst.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
