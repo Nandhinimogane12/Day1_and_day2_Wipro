9ac7362e51919d9e4829d57a64a2b9677286b645
Nandhinimogane12 <nandhinimogane74@gmail.com>
2026-05-26 10:07:54 +0530

    Day 29: Add OnlineBookStoreAPI

---
+++ day29/OnlineBookStoreAPI/OnlineBookStoreAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBookStoreAPI.Models;
+
+namespace OnlineBookStoreAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
---
+++ day29/OnlineBookStoreAPI/OnlineBookStoreAPI/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineBookStoreAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int PublicationYear { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
+        public Author? Author { get; set; }
+    }
+}
