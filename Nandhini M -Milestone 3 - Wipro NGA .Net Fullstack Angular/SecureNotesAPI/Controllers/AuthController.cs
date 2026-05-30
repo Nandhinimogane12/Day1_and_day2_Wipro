f657f93fa2dddac343cf3e8be068abdb90cb3aa0
Nandhinimogane12 <nandhinimogane74@gmail.com>
2026-05-30 13:31:57 +0530

    Milestone 3 Secure Notes API

---
+++ Nandhini M -Milestone 3 - Wipro NGA .Net Fullstack Angular/SecureNotesAPI/Controllers/AuthController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecureNotesAPI.Data;
+using SecureNotesAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SecureNotesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _config;
+
+        public AuthController(AppDbContext context,
+            IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            if (_context.Users.Any(u =>
+                u.Username == request.Username))
+            {
+                return BadRequest("Username already exists");
+            }
+
+            var user = new User
+            {
+                Username = request.Username,
+                PasswordHash =
+                BCrypt.Net.BCrypt.HashPassword(request.Password)
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok(new
+            {
+                message =
+                "User registered successfully. Please log in."
+            });
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            var user = _context.Users
+                .FirstOrDefault(u =>
+                u.Username == request.Username);
+
+            if (user == null ||
+                !BCrypt.Net.BCrypt.Verify(
+                    request.Password,
+                    user.PasswordHash))
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name,
+                user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                _config["Jwt:Key"]));
+
+            var creds =
+                new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+            var token =
+                new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires:
+                DateTime.Now.AddHours(1),
+                signingCredentials: creds);
+
+            return Ok(new
+            {
+                token =
+                new JwtSecurityTokenHandler()
+                .WriteToken(token),
+                expires_in = 3600,
+                user = new
+                {
+                    username = user.Username
+                }
+            });
+        }
+    }
+}
