d24910e62e3faaa0f1cbb89f6a24d5f98933f93c
Nandhinimogane12 <nandhinimogane74@gmail.com>
2026-06-08 00:19:43 +0530

    Add day40 EmployeePortal

---
+++ day40/EmployeePortal/src/app/employee/employee.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  standalone: true, 
+  imports: [],
+  templateUrl: './employee.html',
+  styleUrl: './employee.css'
+})
+export class EmployeeComponent { } 
